47e63067dd237f1b868276c8b75c2d46217ab103
Arav Ashish <87805464+Arav2010@users.noreply.github.com>
2021-08-04 11:16:25 +0530

    Add files via upload

---
+++ Nose-Ball Detector.js
@@ -0,0 +1,31 @@
+let video;
+let poseNet;
+let nose;
+
+function setup() {
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.hide();
+  nose = createVector(width/2,height/2);
+  poseNet = ml5.poseNet(video, modelReady);
+  poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(poses){
+  if(poses.length > 0){
+    let posenose = poses[0].pose.nose;
+    nose.x = posenose.x;
+    nose.y =posenose.y;
+  }
+}
+
+function modelReady(){
+  console.log("Model is Ready!!!");
+}
+
+function draw() {
+  background(220);
+  image(video, 0, 0);
+  fill(255,255,0)
+  circle(nose.x, nose.y, 20);
+}
